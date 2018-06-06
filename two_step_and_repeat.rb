88ed28fce9349b0bd496a61e77ad188508ae3eb6
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-06 17:38:11 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -12,10 +12,11 @@ def first_steps
     sleep(0.5)
   end
 end
-first_steps
+# first_steps
 
 def a_few_more_steps
   # Write a loop that outputs the first two sets of steps in the Two-Step
+  first_steps
 end
 
 def how_many_steps?
